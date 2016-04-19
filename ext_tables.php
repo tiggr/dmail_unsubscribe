125c16ffc41c110c32d437ba5ba3c38f236ec13a
Daniel Schöne <daniel@schoene.it>
2016-04-19 15:57:58 +0200

    TYPO3 7.6 LTS compatibility

--- ext_tables.php
+++ ext_tables.php
@@ -6,14 +6,14 @@ if (!defined ('TYPO3_MODE')) {
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
 
 
-t3lib_extMgm::addPlugin(array(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
 	'LLL:EXT:dmail_unsubscribe/locallang_db.xml:tt_content.list_type_pi1',
 	$_EXTKEY . '_pi1',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
 
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'Direct mail unsubscription');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,'static/', 'Direct mail unsubscription');
 
 
 ?>
