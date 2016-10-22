dc50a6656d021c79ce3e7790536c6eaf4176923c
Daniel Schöne <daniel@schoene.it>
2016-10-22 12:55:04 +0200

    Removes obsolete XCLASS feature

--- pi1/class.tx_dmailunsubscribe_pi1.php
+++ pi1/class.tx_dmailunsubscribe_pi1.php
@@ -193,7 +193,3 @@ class tx_dmailunsubscribe_pi1 extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
     }
 
 }
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dmail_unsubscribe/pi1/class.tx_dmailunsubscribe_pi1.php']) {
-    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dmail_unsubscribe/pi1/class.tx_dmailunsubscribe_pi1.php']);
-}
