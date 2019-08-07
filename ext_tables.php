d7ae60997a34b0ee5cfb4ace3a7c82f2a142733e
Silvio Kranzusch <sk@davitec.de>
2019-08-07 10:56:35 +0200

    cleanup deprecation log

--- ext_tables.php
+++ ext_tables.php
@@ -9,7 +9,7 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
 	'LLL:EXT:dmail_unsubscribe/locallang_db.xml:tt_content.list_type_pi1',
 	$_EXTKEY . '_pi1',
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif'
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
 
 
