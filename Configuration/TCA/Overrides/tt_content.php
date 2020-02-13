25441be4508bb602898b90e38df47946b3c0c3f1
Daniel Schöne <daniel@schoene.it>
2020-02-13 16:32:26 +0100

    Use $GLOBALS['TCA'] not $TCA

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,6 @@
 <?php defined('TYPO3_MODE') or die ('Access denied.');
 
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['dmail_unsubscribe_pi1']='layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['dmail_unsubscribe_pi1']='layout,select_key';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
     'LLL:EXT:dmail_unsubscribe/locallang_db.xml:tt_content.list_type_pi1',
