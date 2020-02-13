c06a50fd884c6c7955902ed2920360b3e4e78ac4
Daniel Schöne <daniel@schoene.it>
2020-02-13 18:23:48 +0100

    Fix configuration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,13 @@
 <?php defined('TYPO3_MODE') or die ('Access denied.');
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['dmail_unsubscribe_pi1']='layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['dmail_unsubscribe_pi1'] = 'layout,select_key';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
-    'LLL:EXT:dmail_unsubscribe/locallang_db.xml:tt_content.list_type_pi1',
-    'dmail_unsubscribe' . '_pi1',
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('dmail_unsubscribe') . 'ext_icon.gif'
-),'list_type');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'LLL:EXT:dmail_unsubscribe/locallang_db.xml:tt_content.list_type_pi1',
+        'dmail_unsubscribe' . '_pi1',
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('dmail_unsubscribe') . 'ext_icon.gif',
+    ],
+    'list_type',
+    'dmail_unsubscribe'
+);
